5a52141e580f6b60bffb57dc499961aaaa29cf50
Ian Alexander <ian.alexander@octoenergy.com>
2024-06-06 15:14:54 +0100

    New version with release process

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ The calculation to generate a smart building rating
 
 ## Setup
 - Run `make setup` which will run all the required commands
+- Run `pipenv run python smart-building-rating-calculator/main.py`
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smart-buildings-rating-calculator",
-    version="0.1.0",
+    version="0.2.0",
     author="CentreForNetZero",
     author_email="data@centrefornetzero.org",
     description="The calculation to generate a smart building rating",
