68fc65f7bebc92fd88513cc6b5724a436988ee56
ianlmgoddard <ian.goddard@centrefornetzero.org>
2024-07-29 15:34:07 +0100

    move exports to correct folder

---
+++ src/smart_building_rating_calculator/__init__.py
@@ -0,0 +1,17 @@
+from .calculate_sbr_score import sbr_score
+from .inputs import (
+    BatterySize,
+    EVChargerPower,
+    HeatingSource,
+    HotWaterSource,
+    SolarInverterSize,
+)
+
+__all__ = [
+    "sbr_score",
+    "BatterySize",
+    "EVChargerPower",
+    "HeatingSource",
+    "HotWaterSource",
+    "SolarInverterSize",
+]
